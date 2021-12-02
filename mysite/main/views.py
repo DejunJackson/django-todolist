b58dce6fbfe206cd2f70bffef31e7f516ef85ba5
Dejun Jackson <dejunjackson16@gmail.com>
2021-12-02 01:25:08 -0600

    add user specific todolists

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -9,25 +9,28 @@ from .forms import CreateNewList
 
 def index(request, id):
     ls = ToDoList.objects.get(id=id)
-    if request.method == "POST":
-        if request.POST.get("save"):
-            for item in ls.item_set.all():
-                if request.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+
+    if ls in request.user.todolist.all():
+
+        if request.method == "POST":
+            if request.POST.get("save"):
+                for item in ls.item_set.all():
+                    if request.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()
+
+            elif request.POST.get("newItem"):
+                txt = request.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
-                item.save()
+                    print("invalid")
 
-        elif request.POST.get("newItem"):
-            txt = request.POST.get("new")
-
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
-
-    return render(request, "main/list.html", {"ls": ls})
-
+        return render(request, "main/list.html", {"ls": ls})
+    return render(request, "main/view.html", {})
 def home(request):
     return render(request, "main/home.html", {})
 
@@ -40,11 +43,16 @@ def create(request):
 
         if form.is_valid():
             n = form.cleaned_data["name"]
-            t = ToDoList(name=n)
+            t=ToDoList(name=n)
             t.save()
+            request.user.todolist.add(t)
 
         return HttpResponseRedirect("/%i" %t.id)
 
     else:
         form = CreateNewList()
     return render(request, "main/create.html", {"form":form})
+
+
+def view(request):
+    return render(request, "main/view.html", {})
